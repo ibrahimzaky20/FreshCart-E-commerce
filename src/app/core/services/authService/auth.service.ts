d3acf996bd693c41d724c27291063c72108ade23
ibrahimzaky20 <ibrahimzaky835@gmail.com>
2025-09-06 04:27:58 +0300

    third commit

--- src/app/core/services/authService/auth.service.ts
+++ src/app/core/services/authService/auth.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment.development';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private httpClient :HttpClient = inject(HttpClient);
+  registerApi(bodyData: object): Observable<any> {
+    return this.httpClient.post(`${environment.baseUrl}auth/signup`, bodyData);
+  }
+  }
   
-}
+
--- src/app/features/auth/registerComponent/register.component.ts
+++ src/app/features/auth/registerComponent/register.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import{ReactiveFormsModule,FormControl, FormGroup,Validators, AbstractControl}from '@angular/forms'
+import { AuthService } from '../../../core/services/authService/auth.service';
+import { Router } from '@angular/router';
+
+
+
 
 @Component({
   selector: 'app-register',
@@ -8,16 +13,34 @@ import{ReactiveFormsModule,FormControl, FormGroup,Validators, AbstractControl}fr
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  private auth: AuthService=inject(AuthService);
+  private router: Router=inject(Router);
+  errorMessage =signal<string>('');
+  isLoading=signal<boolean>(false);
 
   registerForm:FormGroup=new FormGroup({
     name:new FormControl(null,[Validators.required,Validators.minLength(3),Validators.maxLength(10)]),
     email:new FormControl(null,[Validators.required,Validators.email]),
-    password:new FormControl(null,[Validators.required,Validators.pattern(/^[A-Z][a-z0-9]{3,8}$/)]),
-    rePassword:new FormControl(null,[Validators.required,Validators.pattern(/^[A-Z][a-z0-9]{3,8}$/)]),
+    password:new FormControl(null,[Validators.required,Validators.pattern(/^[A-Z][a-z0-9]{6}$/)]),
+    rePassword:new FormControl(null,[Validators.required,Validators.pattern(/^[A-Z][a-z0-9]{6}$/)]),
     phone:new FormControl(null,[Validators.required,Validators.pattern(/^01[0125][0-9]{8}$/)]),
   },this.confirmPassword)
   onSubmit(){
-    if(this.registerForm.valid){console.log(this.registerForm.value);}}
+    if(this.registerForm.valid){
+      this.isLoading.set(true);
+      this.auth.registerApi(this.registerForm.value).subscribe({
+        next:(res)=>{if(res.message=='success'){
+          this.router.navigate(['/login'])
+          this.isLoading.set(false);
+        }},
+        error:(err)=>{
+          this.errorMessage.set(err.error.message)
+          this.isLoading.set(false);
+        },
+        complete:()=>{}
+      })
+    }
+  }
 
     confirmPassword(g:AbstractControl){
       if(g.get('password')?.value===g.get('rePassword')?.value){
